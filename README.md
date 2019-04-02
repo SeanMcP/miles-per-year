78dfc48de0d5e2de403bebec2ad0e0e98e7232de
Sean McPherson <seanmcp25@gmail.com>
2019-04-02 09:12:21 -0400

    Remove boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This is your Chrome extension which replaces a word or phrase with a different word or phrase. 
+# Miles Per Year
+
+Make the used car search easier by adding a "miles per year" field on inidividual listings.
+
+Forked from: [How-to: Make your own text-replacing Chrome extension like ‘Millennials to Snake People’](https://9to5google.com/2015/06/14/how-to-make-a-chrome-extensions/) by [Tom Maxwell](https://twitter.com/tomaxwell)
--- content.js
+++ content.js
@@ -1,18 +1,21 @@
-var elements = document.getElementsByTagName('*');
+// var elements = document.getElementsByTagName("*");
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+// for (var i = 0; i < elements.length; i++) {
+//   var element = elements[i];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+//   for (var j = 0; j < element.childNodes.length; j++) {
+//     var node = element.childNodes[j];
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/[word or phrase to replace here]/gi, '[new word or phrase]');
+//     if (node.nodeType === 3) {
+//       var text = node.nodeValue;
+//       var replacedText = text.replace(
+//         /[word or phrase to replace here]/gi,
+//         "[new word or phrase]"
+//       );
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
-    }
-}
+//       if (replacedText !== text) {
+//         element.replaceChild(document.createTextNode(replacedText), node);
+//       }
+//     }
+//   }
+// }
